f56fb133ce563f40b964f2b2e991bff9f5599dc5
ramzan1813 <ramzanniazi85@outlook.com>
2023-01-26 17:24:57 +0500

    frontent end react

---
+++ src/config.js
@@ -0,0 +1,23 @@
+const dev = {
+  baseURL: "http://localhost:3080/api/",
+  landingPageUrl: "http://localhost:3080",
+  stripe: {
+    free: "price_1MQ9iZEKbjlHjR0pi3PIUsIc",
+    entry: "price_1MQ9S1EKbjlHjR0p3sSrbAEO",
+    pro: "price_1MQ9gQEKbjlHjR0pLyeXwktF",
+  },
+};
+
+const prod = {
+  baseURL: "/api/",
+  landingPageUrl: "https://app.openaitemplate.com",
+  stripe: {
+    free: "price_1MQ9iZEKbjlHjR0pi3PIUsIc",
+    entry: "price_1MQ9S1EKbjlHjR0p3sSrbAEO",
+    pro: "price_1MQ9gQEKbjlHjR0pLyeXwktF",
+  },
+};
+
+const config = process.env.NODE_ENV === "development" ? dev : prod;
+
+export default config;
